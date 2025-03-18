21c764eadeb90cb22eda0b9664dfd94033da1bdc
iamyh032 <iamyh032@gmail.com>
2025-03-17 17:50:53 -0700

    25/03/17

---
+++ 9613.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int GCD(int a, int b){
+
+    while(a%b!=0){
+        int r = a%b;
+        a = b;
+        b = r;
+    }
+    return b;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        int* arr = new int[n];
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+
+        long int sum = 0;
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                sum += GCD(arr[i], arr[j]);
+            }
+        }
+        
+        cout << sum << '\n';
+    }
+}
