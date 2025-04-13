4c20de9489915b3e7c5924013917cdac2ae97ff9
iamyh032 <iamyh032@gmail.com>
2025-04-12 18:32:07 -0700

    25/04/12

---
+++ 15657.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[10];
+int result[10];
+void func(int index, int current, int n, int m){
+    if(index == m){
+        for(int i=0; i<index; i++){
+            cout << result[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for(int i=current; i<n; i++){
+        result[index] = arr[i];
+        func(index+1, i, n, m);
+    }
+    return;
+}
+
+int main(){
+    int N, M;
+    cin >> N >> M;
+
+    for(int i=0; i<N; i++){
+        cin >> arr[i];
+    }
+
+    sort(arr, arr+N);
+    func(0, 0, N, M);
+}
