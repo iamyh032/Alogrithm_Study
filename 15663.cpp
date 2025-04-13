d1f173c27acf44f49baf8af70a4a9841b9bad33b
iamyh032 <iamyh032@gmail.com>
2025-04-13 14:38:05 -0700

    25/04/13

---
+++ 15663.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[10];
+int result[10] = {};
+bool isUsed[10] = {};
+
+void func(int index, int n, int m){
+    if(index == m){
+        for(int i=0; i<index; i++){
+            cout << result[i] << " ";
+        }
+        cout << "\n";
+        return;
+    } else {
+        for(int i=0; i<n; i++){
+            if(isUsed[i]) {
+                continue;
+            } 
+            isUsed[i] = true;
+            result[index] = arr[i];
+            func(index+1, n, m);
+            isUsed[i] = false;
+            while(i+1<n && arr[i+1]==arr[i]){
+                i++;
+            }
+        }
+        return;
+    }
+}
+
+int main(){
+    int N, M;
+    cin >> N >> M;
+    for(int i=0; i<N; i++){
+        cin >> arr[i];
+    }
+    sort(arr, arr+N);
+    func(0, N, M);
+}
