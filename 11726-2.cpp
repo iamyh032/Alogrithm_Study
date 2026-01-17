2e856b4e5911d00b6e25654e5d9f4a2e873dc3c3
iamyh032 <iamyh032@gmail.com>
2026-01-18 00:33:56 +0900

    26/01/18

---
+++ 11726-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int dp[1001] = {};
+    dp[1]=1;
+    dp[2]=2;
+    int n;
+    cin >> n;
+    for(int i=3; i<=n; i++){
+        dp[i] = dp[i-1]+dp[i-2];
+        dp[i] = dp[i] % 10007;
+    }
+
+    cout << dp[n];
+}
