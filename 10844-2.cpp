31111d728c16eb3d7129d6606d7755e60e49e7e0
iamyh032 <iamyh032@gmail.com>
2026-01-20 15:51:55 +0900

    26/01/20

---
+++ 10844-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int dp[101][19];
+
+int main(){
+    dp[1][0] = 0;
+    for(int i=1; i<10; i++){
+        dp[1][i] = 1;
+    }
+
+    int n;
+    cin >> n;
+    for(int i=2; i<=n; i++){
+        dp[i][0] = dp[i-1][1];
+        for(int j=1; j<9; j++){
+            dp[i][j] = ( dp[i-1][j-1] + dp[i-1][j+1] )%1000000000;
+        }
+        dp[i][9] = dp[i-1][8];
+    }
+
+    int key=0;
+    for(int i=0; i<10; i++){
+        key = (key + dp[n][i])%1000000000;
+    }
+    cout << key << '\n';
+}
